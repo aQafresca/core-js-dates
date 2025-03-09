c60da80a46cf24b0f0b2555415414d92a034113f
ill_Nino <ill_Nino@bk.ru>
2025-03-09 11:48:55 +0300

    feat: add solution part2

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -103,8 +103,12 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const a = new Date(dateStart);
+  const b = new Date(dateEnd);
+  const c = b - a;
+  const millisecondsInADay = 1000 * 60 * 60 * 24;
+  return c / millisecondsInADay + 1;
 }
 
 /**
@@ -124,8 +128,12 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(period, date) {
+  const { end, start } = date;
+  const endParse = Date.parse(end);
+  const startParse = Date.parse(start);
+  const periodParse = Date.parse(period);
+  return startParse <= periodParse && periodParse <= endParse;
 }
 
 /**
@@ -139,8 +147,20 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const d = new Date(date);
+  const year = d.getFullYear();
+  const month = d.getMonth() + 1;
+  const day = d.getUTCDate();
+  let hours = d.getUTCHours();
+  const minutes = d.getMinutes();
+  const seconds = d.getSeconds();
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+  hours %= 12;
+  hours = hours || 12;
+  const formatTime = `${hours}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')} ${ampm}`;
+
+  return `${month}/${day}/${year}, ${formatTime}`;
 }
 
 /**
